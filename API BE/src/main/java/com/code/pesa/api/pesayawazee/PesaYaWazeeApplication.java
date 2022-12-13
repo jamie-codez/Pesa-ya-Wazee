edfb09a1e571ae525d1b375b086ce62311710b82
Jamie Omondi <cruiseomondi90@gmail.com>
2022-12-13 11:04:56 +0300

    Initial commit

---
+++ API BE/src/main/java/com/code/pesa/api/pesayawazee/PesaYaWazeeApplication.java
@@ -0,0 +1,13 @@
+package com.code.pesa.api.pesayawazee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesaYaWazeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesaYaWazeeApplication.class, args);
+	}
+
+}
---
+++ API BE/src/test/java/com/code/pesa/api/pesayawazee/PesaYaWazeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.pesa.api.pesayawazee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesaYaWazeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
